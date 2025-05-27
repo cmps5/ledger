fd27bc7a5dae948ee9a0adc5b50e7b5d91a24e21
Carlos Santos <cmps.5@hotmail.com>
2025-05-27 21:03:05 +0100

    implement fork resolution

--- src/main/java/blockchain/Blockchain.java
+++ src/main/java/blockchain/Blockchain.java
@@ -1,18 +1,31 @@
 package blockchain;
 
+import kademlia.Kademlia;
+
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Stack;
 
 public class Blockchain {
     // The hash of the first block in the blockchain (genesis block) - 64 zeros (256 bits ÷ 4 bits)
     public static final String GENESIS_PREV_HASH = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
     private static final int DIFFICULTY = 1;
+    private static final int CONFLICT_DEPTH = 5;
 
     private static Blockchain instance;
-    private ArrayList<Block> chain;
+    private List<Block> chain;
+    private List<List<Block>> conflictChains;
+
+    private Kademlia kademlia;
+
 
     public Blockchain() {
-        chain = new ArrayList<>();
+        this.chain = new ArrayList<>();
+        this.conflictChains = new ArrayList<>();
+
+        this.kademlia = Kademlia.getInstance();
     }
 
     public static Blockchain getInstance() {
@@ -29,11 +42,11 @@ public class Blockchain {
         return chain.get(chain.size() - 1);
     }
 
-    public ArrayList<Block> getBlockchain (){
-        return this.chain;
+    public ArrayList<Block> getBlockchain() {
+        return (ArrayList<Block>) this.chain;
     }
 
-    public long getChainSize(){
+    public long getChainSize() {
         return this.chain.size();
     }
 
@@ -101,6 +114,104 @@ public class Blockchain {
     public boolean proofOfWork(Block block) {
         String zeros = new String(new char[DIFFICULTY]).replace('\0', '0');
         return !block.getHash().substring(0, DIFFICULTY).equals(zeros);
+    }
+
+    public boolean searchBlock(Block block) {
+        boolean blockExists = false;
+        if (conflictChains.isEmpty()) {
+            return chain.contains(block);
+        } else {
+            for (List<Block> chain : conflictChains) {
+                if (chain.contains(block))
+                    blockExists = true;
+            }
+        }
+        return blockExists;
+    }
+
+    public void storeBlock(Block block) {
+        if (validateBlock(block)) {
+            tryResolveForks();
+        }
+    }
+
+    public synchronized void tryResolveForks() {
+        Stack<List<Block>> forksToClose = new Stack<>();
+        sortAllListsBySize();
+        if (!conflictChains.isEmpty()) {
+            int biggestForkSize = this.conflictChains.get(0).size();
+            for (List<Block> fork : this.conflictChains) {
+                if (biggestForkSize - fork.size() > CONFLICT_DEPTH) {
+                    forksToClose.add(fork);
+                }
+            }
+            if (!forksToClose.isEmpty()) {
+                while (!forksToClose.isEmpty()) {
+                    handleForkTerminate(forksToClose.pop());
+                }
+            } else {
+                System.out.println("No forks can be resolved");
+            }
+        }
+    }
+
+    public synchronized void handleForkTerminate(List<Block> fork) {
+        for (Block blockToDiscard : fork) {
+            if (!conflictChains.get(0).contains(blockToDiscard)) {
+                //Block not in the main chain
+                try {
+                    if (addBlockFromClosedForkPOW(blockToDiscard)) {
+                        kademlia.storeBlock(blockToDiscard);
+                    }
+                } catch (NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        this.conflictChains.remove(fork);
+    }
+
+    public synchronized boolean addBlockFromClosedForkPOW(Block block) throws NoSuchAlgorithmException {
+        if (conflictChains.isEmpty()) {
+            if (this.chain.isEmpty()) {
+                block.setPreviousHash(GENESIS_PREV_HASH);
+            } else {
+                block.setPreviousHash(getLastBlockHash(this.chain));
+            }
+        } else {
+            sortAllListsBySize();
+            //Pick the one that comes first
+            String prevHash = getLastBlockHash(conflictChains.get(0));
+            block.setPreviousHash(prevHash);
+            block.generateHash();
+        }
+
+        //Mine Block
+        Block minedBlock = mine(block);
+        if (validateBlock(minedBlock)) {
+            //Add Mined Block
+            addLocalMinedBlock(minedBlock);
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized void addLocalMinedBlock(Block newMinedBlock) {
+        if (conflictChains.isEmpty()) {
+            this.chain.add(newMinedBlock);
+        } else {
+
+            conflictChains.get(0).add(newMinedBlock);
+            sortAllListsBySize();
+            tryResolveForks();
+        }
+    }
+
+    public synchronized void sortAllListsBySize() {
+        if (this.conflictChains != null && !this.conflictChains.isEmpty()) {
+            this.conflictChains.sort(Comparator.comparingInt((List<Block> l) -> l.size()).reversed());
+            this.chain = this.conflictChains.get(0);
+        }
     }
 
     @Override
